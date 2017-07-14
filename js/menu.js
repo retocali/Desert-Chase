c0f32e16c2b7fa65a6fc78a009307c17cb8c4ad9
6S08 <6S08@dhcp-18-111-9-22.dyn.mit.edu>
2017-07-14 15:44:15 -0400

    resizing

--- js/menu.js
+++ js/menu.js
@@ -2,19 +2,19 @@ var menuState = {
 
 	create: function() {
 
-	var nameLabel = game.add.text(game.world.centerX - gameX/5,game.world.centerY - gameY/10 ,"Road Warrior", {font: "50px Arial", fill: "#0ff0ff"});
+	var nameLabel = game.add.text(game.world.centerX - gameX/5,game.world.centerY - gameY/3.7 ,"Road Warrior", {font: "64px Arial", fill: "#0ff0ff"});
 	console.log("Menued...");
 
 	// Start button
-	gameStart = game.add.button(game.world.centerX - 200, game.world.centerY + 50, 'start', startClick, this);
+	gameStart = game.add.button(game.world.centerX - 200, game.world.centerY - gameY/5.5, 'start', startClick, this);
 	gameStart.scale.setTo(0.25,0.25);
 
 	// Credit button
-	credit = game.add.button(game.world.centerX - 200, game.world.centerY + 200, 'credit', creditClick, this)
+	credit = game.add.button(game.world.centerX - 200, game.world.centerY, 'credit', creditClick, this)
 	gameStart.scale.setTo(0.25,0.25);
 
 	// Instruction button
-	credit = game.add.button(game.world.centerX, game.world.centerY + 200, 'instruction', instructionClick, this)
+	credit = game.add.button(game.world.centerX, game.world.centerY, 'instruction', instructionClick, this)
 	gameStart.scale.setTo(0.25,0.25);
 
 	function startClick() {
