f3a7e6b024203f1ae21d43f74352dd71241d9ce7
6S08 <6S08@dhcp-18-111-4-126.dyn.mit.edu>
2017-07-18 15:31:23 -0400

    everything so far oftuestday

--- js/lose.js
+++ js/lose.js
@@ -4,11 +4,13 @@ var loseState = {
 
         console.log("Lose!");
 
-		var loseText = game.add.text(game.world.centerX - gameX/5,game.world.centerY - TILE_SIZE*5  ,"Click on the Game Over to restart the game", {font: "20px Arial", fill: "#0ff0ff"});
+		var loseText = game.add.text(game.world.centerX, game.world.centerY + TILE_SIZE *3, "Click on the Game Over to restart the game", {font: "20px Arial", fill: "#0ff0ff"});
+		loseText.anchor.setTo(0.5,0.5);
 
 		// Start button
-		gameStart = game.add.button(game.world.centerX - 5*TILE_SIZE, game.world.centerY -5*TILE_SIZE , 'loseScreen', loseClick, this);
+		gameStart = game.add.button(game.world.centerX, game.world.centerY, 'loseScreen', loseClick, this);
 		gameStart.scale.setTo(0.75,0.75);
+		gameStart.anchor.setTo(0.5,0.5);
 
 		loseMusic();
 
--- js/win.js
+++ js/win.js
@@ -4,11 +4,13 @@ var winState = {
 
         console.log("Win!");
 
-		var winText = game.add.text(game.world.centerX - gameX/5,game.world.centerY - TILE_SIZE*5,"Click on the You Win to start new level", {font: "20px Arial", fill: "#0ff0ff"});
+		var winText = game.add.text(game.world.centerX, game.world.centerY + TILE_SIZE*3,"Click on the You Win to start new level", {font: "20px Arial", fill: "#0ff0ff"});
+		winText.anchor.setTo(0.5,0.5);
 
 		// Start button
-		gameStart = game.add.button(game.world.centerX - 5.5*TILE_SIZE, game.world.centerY -5*TILE_SIZE , 'winScreen', winClick, this);
+		gameStart = game.add.button(game.world.centerX , game.world.centerY, 'winScreen', winClick, this);
 		gameStart.scale.setTo(0.75,0.75);
+		gameStart.anchor.setTo(0.5,0.5);
 
 		winMusic();
 
